8eb2f73cea704f5c775fbdf73abc1730aab8031b
Bhargav845 <bhargavchowdary845@gmail.com>
2020-04-25 11:06:25 +0530

    Intial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function($) {
+     var typed = new Typed("span.text-rotate", {
+          strings: ["i'm the one", "ok we can do it"],
+          typespeed: 100,
+          backspeed: 100,
+          fadeout: false,
+          smartbackspace: true,
+          loop: true
+     });
+})(jQuery);
+
+(function($) {
+     var typed = new Typed('a.text-rotate', {
+          strings: ["Every thing is possible"],
+          typespeed: 100,
+          backspeed: 100,
+          fadeout: false,
+          smartbackspace:true,
+          loop: true
+     });
+})(jQuery);
+
